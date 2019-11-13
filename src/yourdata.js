6eb9ee5eea7eabacf5e231b70e0f000696c608fa
Mariana Rascon <51216146+Nienorloth@users.noreply.github.com>
2019-11-13 15:43:12 -0600

    woods background image

---
+++ src/yourdata.js
@@ -0,0 +1,80 @@
+export default
+    {
+        //(Please Do Not Remove The comma(,) after every variable)
+        //Change The Website Template
+        name :'Mariana Rascon Montaño',
+        headerTagline: [//Line 1 For Header
+                        'Hi, I am Mariana Rascon Montaño',
+                        //Line 2 For Header
+                        'Front-End Developer ',
+                        //Line 3 For Header
+                        
+    ],
+        //Contact Email
+        contactEmail:'nienorloth@gmail.com',
+        // Add Your About Text Here
+        abouttext: "My  name is Mariana. I live in Guadalajara, Mexico. I work with Javascript, HTML and CSS to create web applications, always looking for excellence. I have some experience with React, Angular and node.js. I am passionate about my work and very commited when it comes to collaborative working. I enjoy reading and spending time outdoors with my family. I am looking forward to get involved in new projects and continue learning.",
+        aboutImage:'https://firebasestorage.googleapis.com/v0/b/cuaderno-de-colectas.appspot.com/o/port1.jpg?alt=media&token=bf9f5652-de52-4383-90a1-f9fe9e902b1e',
+       //Change This To Hide The Image of About Section (True Or False)
+       ShowAboutImage:true,//true or false (Change Here)
+       // Change Projects Here 
+       projects:[
+           {
+            id: 1,//DO NOT CHANGE THIS (Please)😅
+            title:'League of Legends Champion Archive', //Project Title - Add Your Project Title Here
+             service:'Vanilla JS web development', // Add Your Service Type Here
+            //Project Image - Add Your Project Image Here
+             imageSrc:"https://firebasestorage.googleapis.com/v0/b/cuaderno-de-colectas.appspot.com/o/lol.png?alt=media&token=a3f2cd5b-d581-47b6-918c-f105120db462",
+             //Project URL - Add Your Project Url Here
+             url:'https://nienorloth.github.io/GDL003-data-lovers/src/',
+             github: 'https://github.com/Nienorloth/GDL003-data-lovers'
+            },
+            {
+                id: 2,//DO NOT CHANGE THIS (Please)😅
+                title: 'Collector´s notebook',
+                service: 'React Website Development',
+                imageSrc: "https://firebasestorage.googleapis.com/v0/b/cuaderno-de-colectas.appspot.com/o/labnotes.png?alt=media&token=b8f56740-32ed-451e-a2b7-33c49cffbe9b",
+                url: 'https://github.com/Nienorloth/Cuaderno-de-colecta',
+                github: 'https://github.com/Nienorloth/GDL003-data-lovers'
+            },
+            { 
+                id: 3,//DO NOT CHANGE THIS (Please)😅
+                title: 'Social network: School comunity',
+                service: 'Vanilla JS Web App',
+                imageSrc: "https://firebasestorage.googleapis.com/v0/b/cuaderno-de-colectas.appspot.com/o/logo.gif?alt=media&token=fc2e78b3-8642-43d3-9b00-c373999473c3",
+                url: 'https://nienorloth.github.io/GDL003-social-network/src/',
+                github: 'https://github.com/Nienorloth/GDL003-social-network'
+            },
+            {
+                id: 4,//DO NOT CHANGE THIS (Please)😅
+                title: 'Imparatus e-commerce',
+                service: 'Angular web development',
+                imageSrc: "https://firebasestorage.googleapis.com/v0/b/cuaderno-de-colectas.appspot.com/o/imp.png?alt=media&token=2f636f6b-83d5-4ca2-9a77-96a9867fdc3a",
+                url: 'https://imparatus-bf272.firebaseapp.com/',
+                github: 'https://github.com/Nienorloth/GDL003-personal-project'
+           }
+
+                    /*
+
+                    If You Want To Add More Project just Copy and Paste This At The End (Update the id Respectively)
+                ,{
+                id: 5,
+                title: 'Project Five',
+                service: 'Something Amazing',
+                imageSrc: "",
+                url: ''
+            }
+                */
+        ],
+        social: [
+            // Add Or Remove The Link Accordingly
+            {   name:'Github',
+                url:'https://github.com/Nienorloth'},
+            {
+                name: 'LinkedIn',
+                url: 'https://www.linkedin.com/in/mariana-rascon'
+            }
+            
+
+        ]
+    }
