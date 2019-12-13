a5953f9e9c7a5e849ac6c738be3a4e2c36af76e9
Nienorloth <nienorloth@gmail.com>
2019-12-13 11:34:48 -0600

    added cv download

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,6 +3,7 @@ import Fade from 'react-reveal/Fade';
 import data from '../yourdata';
 import github from './../img/github.png';
 import linkedin from './../img/link.png';
+import CV from './../img/Mariana Rascón Montaño.pdf';
 
 class Contact extends Component {
     state = {  }
@@ -14,11 +15,12 @@ class Contact extends Component {
         <Fade bottom>
         <div className='contact-content'>
                     <a href={`mailto:${data.contactEmail}`} className='email'>{data.contactEmail}</a>
+                    <a className="cv" href={CV} download><p>Download CV</p></a>
+
                 <ul>
                     
                         <li><a className="toWhite" target='_blank' rel="noopener noreferrer" href='https://github.com/Nienorloth'><img className="GithubLogo" src={github}/></a></li>
                         <li><a className="toWhite" target='_blank' rel="noopener noreferrer" href='https://www.linkedin.com/in/mariana-rascon'><img className="LinkedInLogo" src={linkedin}/></a></li>
-
                 </ul>
         </div>
         </Fade>
