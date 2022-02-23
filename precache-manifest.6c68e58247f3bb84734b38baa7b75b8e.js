48a6ba399b86b7914d4ac4b05e62dedcbae6a296
Nienorloth <nienorloth@gmail.com>
2022-02-22 20:07:37 -0600

    Updates

---
+++ precache-manifest.6c68e58247f3bb84734b38baa7b75b8e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a975d2d95cbb8bdd10058d0b632e1b84",
+    "url": "/kit-del-viajero/index.html"
+  },
+  {
+    "revision": "fcfd6eabdaea1233ca39",
+    "url": "/kit-del-viajero/static/css/main.67ad3d76.chunk.css"
+  },
+  {
+    "revision": "cc543c85d1ae157bf8dc",
+    "url": "/kit-del-viajero/static/js/2.a75e8edd.chunk.js"
+  },
+  {
+    "revision": "fcfd6eabdaea1233ca39",
+    "url": "/kit-del-viajero/static/js/main.79b39b1f.chunk.js"
+  },
+  {
+    "revision": "222d5a400bdfa747b662",
+    "url": "/kit-del-viajero/static/js/runtime~main.cf5a7b7e.js"
+  },
+  {
+    "revision": "97ef49dedfef01e99aec01787cc20167",
+    "url": "/kit-del-viajero/static/media/moon2.97ef49de.jpg"
+  }
+]);
