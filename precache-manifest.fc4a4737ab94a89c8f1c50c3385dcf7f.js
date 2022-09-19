589d331b96fd6e348698a815fd8d6c5f69818911
Nienorloth <nienorloth@gmail.com>
2022-09-19 11:22:27 -0500

    Updates

---
+++ precache-manifest.fc4a4737ab94a89c8f1c50c3385dcf7f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34da706da95ddfb88925e6c5a5f8ddd4",
+    "url": "/tu-ambulancia/index.html"
+  },
+  {
+    "revision": "6831ff15a563331c1c96",
+    "url": "/tu-ambulancia/static/css/main.c8480e5f.chunk.css"
+  },
+  {
+    "revision": "f60ec20ce270fad49f5e",
+    "url": "/tu-ambulancia/static/js/2.67397f2a.chunk.js"
+  },
+  {
+    "revision": "6831ff15a563331c1c96",
+    "url": "/tu-ambulancia/static/js/main.f4dfaa16.chunk.js"
+  },
+  {
+    "revision": "2e8ae6d097f6def40630",
+    "url": "/tu-ambulancia/static/js/runtime~main.2112ba5b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/tu-ambulancia/precache-manifest.ca6ec28916d6e0b3a69d671ff56c0065.js"
+  "/tu-ambulancia/precache-manifest.fc4a4737ab94a89c8f1c50c3385dcf7f.js"
 );
 
 self.addEventListener('message', (event) => {
