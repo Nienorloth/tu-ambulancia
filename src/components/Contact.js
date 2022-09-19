1c45cf515ac3b95efc43a661527d1b5174723882
Nienorloth <nienorloth@gmail.com>
2022-09-19 10:08:31 -0500

    first

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,9 +1,7 @@
 import React, { Component } from 'react';
 import Fade from 'react-reveal/Fade';
 import data from '../yourdata';
-import github from './../img/github.png';
-import linkedin from './../img/link.png';
-import CV from './../img/Resume Mariana Rascón Montaño.pdf';
+
 
 class Contact extends Component {
     state = {  }
@@ -17,11 +15,6 @@ class Contact extends Component {
                     <a href={`mailto:${data.contactEmail}`} className='email'>{data.contactEmail}</a>
                     <a className="cv" href={CV} download><p>Download CV</p></a>
 
-                <ul>
-                    
-                        <li><a className="toWhite" target='_blank' rel="noopener noreferrer" href='https://github.com/Nienorloth'><img className="GithubLogo" src={github}/></a></li>
-                        <li><a className="toWhite" target='_blank' rel="noopener noreferrer" href='https://www.linkedin.com/in/mariana-rascon'><img className="LinkedInLogo" src={linkedin}/></a></li>
-                </ul>
         </div>
         </Fade>
 
